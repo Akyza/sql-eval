8b4d010cd83ab08c49eb19df604cf20532b66a77
Adrien <libegon78@hotmail.fr>
2018-11-30 14:44:54 +0100

    insert correctif

--- seed.sql
+++ seed.sql
@@ -2,17 +2,16 @@ insert into class (nom) values ('D20.1');
 insert into class (nom) values ('D20.2');
 insert into class (nom) values ('D20.4');
 
-insert into student (nom,premon,mail,class) values ('Tavares','Bryce','Tavares.Bryce@gmail.com','3');
-insert into student ( nom,prenom,mail,class) values ('Stupnicki','Adrien','Stupnicki.Adrien@gmail.com','3');
+insert into student (nom,prenom,mail,class) values ('Tavares','Bryce','Tavares.Bryce@gmail.com','3');
+insert into student (nom,prenom,mail,class) values ('Stupnicki','Adrien','Stupnicki.Adrien@gmail.com','3');
 insert into student (nom,prenom,mail,class) values ('Dupont','Jean','Dupont.Jean@gmail.com','3');
 
-insert into student (nom,premon,mail,class) values ('Beauregard','Jeanne','Beauregard.Jeanne@gmail.com','1');
-insert into student (nom,premon,mail,class) values ('Belata','Alex','Belata.Alex@gmail.com','1');
+insert into student (nom,prenom,mail,class) values ('Beauregard','Jeanne','Beauregard.Jeanne@gmail.com','1');
+insert into student (nom,prenom,mail,class) values ('Belata','Alex','Belata.Alex@gmail.com','1');
 insert into student (nom,premon,mail,class) values ('Gianetto','Alexendre','Gianetto.Alexendre@gmail.com','1');
 
-insert into student (nom,premon,mail,class) values ('Sherl','Elya','Sher.Elya@gmail.com','2');
-insert into student (nom,premon,mail,class) values ('Alvara','Pan','Alvara.Panyce@gmail.com','2');
-insert into student (nom,premon,mail,class) values ('Kenray','Akyza','Kenray.Akyza@gmail.com','2');
+insert into student (nom,prenom,mail,class) values ('Sherl','Elya','Sher.Elya@gmail.com','2');
+insert into student (nom,prenom,mail,class) values ('Alvara','Pan','Alvara.Panyce@gmail.com','2');
+insert into student (nom,prenom,mail,class) values ('Kenray','Akyza','Kenray.Akyza@gmail.com','2');
 
 
-Insert into `ipssi-eval-sql`
