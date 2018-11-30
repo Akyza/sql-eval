bfa1971fe8893adf7ac98786f9804fd7ba3b77bb
Adrien <libegon78@hotmail.fr>
2018-11-30 14:50:24 +0100

    insert correctif3

--- seed.sql
+++ seed.sql
@@ -1,17 +1,17 @@
-insert into id_class (nom) values ('D20.1');
-insert into id_class (nom) values ('D20.2');
-insert into id_class (nom) values ('D20.4');
+insert into class (nom) values ('D20.1');
+insert into class (nom) values ('D20.2');
+insert into class (nom) values ('D20.4');
 
-insert into student (nom,prenom,mail,id_class) values ('Tavares','Bryce','Tavares.Bryce@gmail.com','3');
-insert into student (nom,prenom,mail,id_class) values ('Stupnicki','Adrien','Stupnicki.Adrien@gmail.com','3');
-insert into student (nom,prenom,mail,id_class) values ('Dupont','Jean','Dupont.Jean@gmail.com','3');
+insert into student (nom,prenom,mail,class_id) values ('Tavares','Bryce','Tavares.Bryce@gmail.com','3');
+insert into student (nom,prenom,mail,class_id) values ('Stupnicki','Adrien','Stupnicki.Adrien@gmail.com','3');
+insert into student (nom,prenom,mail,class_id) values ('Dupont','Jean','Dupont.Jean@gmail.com','3');
 
-insert into student (nom,prenom,mail,id_class) values ('Beauregard','Jeanne','Beauregard.Jeanne@gmail.com','1');
-insert into student (nom,prenom,mail,id_class) values ('Belata','Alex','Belata.Alex@gmail.com','1');
-insert into student (nom,premon,mail,id_class) values ('Gianetto','Alexendre','Gianetto.Alexendre@gmail.com','1');
+insert into student (nom,prenom,mail,class_id) values ('Beauregard','Jeanne','Beauregard.Jeanne@gmail.com','1');
+insert into student (nom,prenom,mail,class_id) values ('Belata','Alex','Belata.Alex@gmail.com','1');
+insert into student (nom,prenom,mail,class_id) values ('Gianetto','Alexendre','Gianetto.Alexendre@gmail.com','1');
 
-insert into student (nom,prenom,mail,id_class) values ('Sherl','Elya','Sher.Elya@gmail.com','2');
-insert into student (nom,prenom,mail,id_class) values ('Alvara','Pan','Alvara.Panyce@gmail.com','2');
-insert into student (nom,prenom,mail,id_class) values ('Kenray','Akyza','Kenray.Akyza@gmail.com','2');
+insert into student (nom,prenom,mail,class_id) values ('Sherl','Elya','Sher.Elya@gmail.com','2');
+insert into student (nom,prenom,mail,class_id) values ('Alvara','Pan','Alvara.Panyce@gmail.com','2');
+insert into student (nom,prenom,mail,class_id) values ('Kenray','Akyza','Kenray.Akyza@gmail.com','2');
 
 
